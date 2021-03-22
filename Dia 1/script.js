58a9c1ee02a0deef57274f2c2e410605d336c577
cecilia.oliveira <cissa-fr-dev@users.github.com>
2021-03-22 17:22:13 -0300

    first commit

---
+++ Dia 1/script.js
@@ -0,0 +1,19 @@
+const calcularReal = document.getElementById('calcularReal');
+const calcularDolar = document.getElementById('calcularDolar');
+
+function converteValor() {
+    const valorEmReal = document.getElementById('real').value;
+    const valorEmDolar = document.getElementById('dolar').value;
+    const resultado = document.getElementById('resultado');
+
+    if (valorEmReal) {
+        resultado.textContent = "O valor em dolar é: U$" + parseFloat(valorEmReal * 5.50).toFixed(2).replace('.', ',');
+    } if (valorEmDolar) {
+        resultado.textContent = "O valor em reais é: R$" + parseFloat(valorEmDolar / 5.50).toFixed(2).replace('.', ',');
+    } if (!valorEmReal && !valorEmDolar) {
+        resultado.textContent = "Nenhum campo preenchido!";
+    }
+}
+
+calcularReal.addEventListener('click', converteValor);
+calcularDolar.addEventListener('click', converteValor);
